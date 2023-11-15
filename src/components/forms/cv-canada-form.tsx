26b1f78ed4f6eb5d93b1d1ab89d936ad5f874171
abdellah <Abdellah.chehri14@gmail.com>
2023-11-15 10:56:26 +0100

    add

---
+++ src/components/forms/cv-canada-form.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { type FC } from "react";
+
+interface CvCanadaFormAbdullahProps {}
+
+const CvCanadaFormAbdullahProps: FC = ({}) => {
+  return <div className="w-full "></div>;
+};
+
+export default CvCanadaFormAbdullahProps;
