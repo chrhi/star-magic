def779cfa9acb67b2d2978e163ce5103f19d6956
abdellah <Abdellah.chehri14@gmail.com>
2023-11-17 10:11:24 +0100

    push to main

--- src/components/layout/header/NoAuthNavBar.tsx
+++ src/components/layout/header/NoAuthNavBar.tsx
@@ -10,9 +10,9 @@ const NoAuthNavBar = () => {
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between ">
           <div className="w-[50%] h-full flex items-center justify-start gap-x-4">
-            <Link href="/" prefetch={false} className="flex z-40 font-semibold">
+            <div className="flex z-40 font-semibold">
               <Image alt="logo" src="/logo.png" width={35} height={35} />
-            </Link>
+            </div>
             {/* <Link href={"/form"}>
               <Button variant="ghost">Services</Button>
             </Link> */}
